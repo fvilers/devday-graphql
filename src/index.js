b10b1d89b4bae30e8a3e29be7f269a9bcf7d8c54
Fabian Vilers <fabian.vilers@dev-one.com>
2017-11-27 16:46:37 +0100

    Add helmet

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import compression from 'compression';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(compression());
+app.use(helmet());
 app.use('*', (req, res) => res.sendStatus(404));
 
 export default app;
