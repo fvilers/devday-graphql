01df071f75da2d69613836fbdc449d517e994666
Fabian Vilers <fabian.vilers@dev-one.com>
2017-11-27 16:48:03 +0100

    Add morgan

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import logger from 'morgan';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(compression());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
 app.use('*', (req, res) => res.sendStatus(404));
 
 export default app;
