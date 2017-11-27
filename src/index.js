42a4333c59b3db9dfe573a0fda1fb1d94ae955b6
Fabian Vilers <fabian.vilers@dev-one.com>
2017-11-27 17:27:15 +0100

    Add graphql middleware

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import compression from 'compression';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
+import graphQLHTTP from 'express-graphql';
 import { errorHandler } from './middlewares';
 import routes from './routes';
+import schema from './schema';
 
 const app = express();
 
@@ -13,6 +15,10 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
+app.use('/graphql', graphQLHTTP({
+  schema,
+  graphiql: true
+}));
 app.use('/', routes());
 app.use('*', (req, res) => res.sendStatus(404));
 app.use(errorHandler());
