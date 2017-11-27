c24fd523b5d265afcbf12abd58cb7eb52246c1ff
Fabian Vilers <fabian.vilers@dev-one.com>
2017-11-27 16:45:43 +0100

    Add compression

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import compression from 'compression';
 
 const app = express();
+
+app.use(compression());
 app.use('*', (req, res) => res.sendStatus(404));
 
 export default app;
