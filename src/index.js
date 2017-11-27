a392b80185d1d6437ce0843c33d28a841c7d9a32
Fabian Vilers <fabian.vilers@dev-one.com>
2017-11-27 16:47:24 +0100

    Add body-parser

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(compression());
 app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('*', (req, res) => res.sendStatus(404));
 
 export default app;
